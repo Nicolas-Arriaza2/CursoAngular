fb524988d83bee517bfc4d07eb1e47bbee4b53de
Nicolás Arriaza Verreni <nicolas.arriaza@xintec.cl>
2022-05-10 17:46:26 -0400

    add input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 interface Tarjeta{
   titulo:string;
   subtitulo:string;
-  nro?: number;
+  image:string;
 }
 
 @Component({
@@ -17,9 +17,9 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
     this.ArregloTargetas = [
-      {titulo:'Video 1', subtitulo: 'Subtitulo 1', nro: 1},
-      {titulo:'Video 2', subtitulo: 'Subtitulo 2'},
-      {titulo:'Video 3', subtitulo: 'Subtitulo 3'}
+      {titulo:'Video 1', subtitulo: 'Subtitulo 1', image:"https://1000marcas.net/wp-content/uploads/2021/10/Bad-Bunny-Logo.png"},
+      {titulo:'Video 2', subtitulo: 'Subtitulo 2',image:"https://ca-times.brightspotcdn.com/dims4/default/cb4ea65/2147483647/strip/true/crop/3094x1940+0+0/resize/1486x932!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F32%2F2e%2F1b80271c4869bd2d3898e02c66f6%2Fnicky.jpg"},
+      {titulo:'Video 3', subtitulo: 'Subtitulo 3',image:"https://www.radiopop.cl/wp-content/uploads/2018/07/50-cent-2.png"}
     ]  } 
 
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -9,6 +9,7 @@ export class CardComponent implements OnInit {
 
   public image:string="https://i.pinimg.com/originals/5d/e4/76/5de4768061c7c1230bdcfb5df181b6e0.jpg"
   public Titulo:string = "Curso de Angular con interpolacion"
+  @Input() dataEntrante:any;
 
   constructor() { }
 
